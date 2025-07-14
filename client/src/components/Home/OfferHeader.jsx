1f03bb495485110b4763f5d21be42b906338c186
shaamilshan <shmlvk123@gmail.com>
2025-07-14 16:51:18 +0530

    Changes on product card,offer header,home2

--- client/src/components/Home/OfferHeader.jsx
+++ client/src/components/Home/OfferHeader.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const OfferHeader = () => {
+  const messages = [
+    "Free shipping on all orders above ₹500",
+    "Sign up now and get 10% off your first order",
+    "Refer a friend and earn rewards!"
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShow(false); // start slide-out
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % messages.length);
+        setShow(true); // start slide-in
+      }, 300); // match animation duration
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="w-full text-xs bg-[#000000] text-white py-4 overflow-hidden relative">
-      <div className="flex justify-between mx-2">
-        <div className="flex gap-2">
-        <div className="border-r pr-2">(09)12345565654</div>
-        <div>Store location</div>
-        </div>
-        <div>Tell a friend about Drou & get 20% off*</div>
-        <div className="flex">
-        <div>USD</div>
-        <div>Login/Signup</div> 
+    <div className="w-full text-xs bg-black text-white py-4 overflow-hidden relative h-6">
+      <div className="relative w-full h-full flex justify-center items-center">
+        <div
+          className={`absolute transition-all duration-300 ease-in-out ${
+            show
+              ? "translate-x-0 opacity-100"
+              : "translate-x-full opacity-0"
+          }`}
+          key={currentIndex}
+        >
+          {messages[currentIndex]}
         </div>
       </div>
     </div>
